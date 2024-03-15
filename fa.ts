8464b7f9b199cbbf4444505ccadcf10a7589e5af
Mustabshira khalid <160775326+Nadeem-59@users.noreply.github.com>
2024-03-16 02:17:42 +0500

    Add files via upload

---
+++ fa.ts
@@ -0,0 +1,4 @@
+ // Ques 4:Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author.
+ //Your Output should look something like the following,including the quotation marks:
+
+console.log('Imran Khan once said, "Absolutely Not"');
